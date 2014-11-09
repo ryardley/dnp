93bcdfe500db84a269bd84b888c9ad24e5a24fca
Rudi Yardley <rudi@theexchangegroup.com.au>
2014-11-09 21:32:33 +1100

    added app name

--- srv/run.sh
+++ srv/run.sh
@@ -3,4 +3,5 @@ run_cmd "docker run \
           -p 80:80 \
           -p 443:443 \
           -v /var/run/docker.sock:/tmp/docker.sock \
-          -v /var/docker/dnp/sites:/etc/nginx/sites-enabled"
+          -v /var/docker/dnp/sites:/etc/nginx/sites-enabled \
+          $APPLICATION_NAME"
