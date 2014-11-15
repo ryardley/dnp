3c04ac4991bca5b1bdd91ca6522ff15c62226186
Rudi Yardley <contact@rudiyardley.com>
2014-11-15 15:29:36 +1100

    Update build.sh

--- srv/build.sh
+++ srv/build.sh
@@ -1,5 +1,7 @@
 source .environment
 mkdir -p /var/docker/dnp/logs/
 WD=$(pwd)
+$CURRENT_SCRIPT=$(ls $SUPERVISOR_PATH/dnp.conf)
+if [ -n "$CURRENT_SCRIPT" ]; then rm $SUPERVISOR_PATH/dnp.conf; fi
 cd $SUPERVISOR_PATH && ln -s $WD/dnp.conf && cd $WD
 run_cmd "docker build -t $APPLICATION_NAME ../"
